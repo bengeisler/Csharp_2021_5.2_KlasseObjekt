befe30e9cb89fcc7ddc0f94c8f4ce2a6f2a85204
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-11 15:16:59 +0100

    Projektdateien hinzufügen.

---
+++ Csharp_2021_5.2_KlasseObjekt/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Csharp_2021_5._2_KlasseObjekt
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblAnzeige = new System.Windows.Forms.Label();
+			this.btnAnzeigen = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblAnzeige
+			// 
+			this.lblAnzeige.AutoSize = true;
+			this.lblAnzeige.Location = new System.Drawing.Point(12, 19);
+			this.lblAnzeige.Name = "lblAnzeige";
+			this.lblAnzeige.Size = new System.Drawing.Size(35, 13);
+			this.lblAnzeige.TabIndex = 0;
+			this.lblAnzeige.Text = "label1";
+			// 
+			// btnAnzeigen
+			// 
+			this.btnAnzeigen.Location = new System.Drawing.Point(261, 19);
+			this.btnAnzeigen.Name = "btnAnzeigen";
+			this.btnAnzeigen.Size = new System.Drawing.Size(75, 23);
+			this.btnAnzeigen.TabIndex = 1;
+			this.btnAnzeigen.Text = "Anzeigen";
+			this.btnAnzeigen.UseVisualStyleBackColor = true;
+			this.btnAnzeigen.Click += new System.EventHandler(this.btnAnzeigen_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(368, 76);
+			this.Controls.Add(this.btnAnzeigen);
+			this.Controls.Add(this.lblAnzeige);
+			this.Name = "Form1";
+			this.Text = "Klasse, Objekt";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblAnzeige;
+		private System.Windows.Forms.Button btnAnzeigen;
+	}
+}
+
---
+++ Csharp_2021_5.2_KlasseObjekt/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_2021_5._2_KlasseObjekt
+{
+	public partial class Form1 : Form
+	{
+		// Objekt "vespa" global deklarieren & erzeugen
+		private Fahrzeug vespa = new Fahrzeug();
+
+		// private Random r = new Random(); // Zufallszahl erzeugen
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnAnzeigen_Click(object sender, EventArgs e)
+		{
+			// Beispiel (bisher):
+			int variable;
+
+			// Variable vom Typ "Fahrzeug" erstellen
+			// Fahrzeug vespa;
+
+			// Objekt vom Typ "Fahrzeug" erstellen und der Variablen zuweisen
+			// vespa = new Fahrzeug();
+
+			// Ursprüngliche Geschwindigkeit ausgeben
+			lblAnzeige.Text = vespa.Ausgabe();
+
+			// Beschleunigen (Aufruf der Methode Beschleunigen)
+			vespa.Beschleunigen(20);
+
+			// Neue Geschwindigkeit ausgeben
+			//lblAnzeige.Text += "\n" + vespa.Ausgabe();
+
+			// Zugriff auf Eigenschaft "Geschwindigkeit"
+			// über die Eigenschaftsmethode "Geschwindigkeit" (lesend)
+			lblAnzeige.Text += "\n" + vespa.Geschwindigkeit.ToString();
+
+			// Funktioniert nicht, da set private ist!
+			// vespa.Geschwindigkeit = 120;
+
+			// Test der Konstruktoren
+			Fahrzeug zündapp = new Fahrzeug(20);
+			Console.WriteLine("Objekt zündapp erstellt:");
+			Console.WriteLine(zündapp.ToString());
+
+			Console.WriteLine("");
+
+			Fahrzeug polo = new Fahrzeug("Mein kleiner Polo");
+			Console.WriteLine("Objekt Polo erstellt:");
+			Console.WriteLine(polo.ToString());
+
+			Console.WriteLine("");
+
+			Fahrzeug honda = new Fahrzeug("Der Honda", 80);
+			Console.WriteLine("Objekt honda erstellt:");
+			Console.WriteLine(honda.ToString());
+
+			Console.WriteLine("");
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
